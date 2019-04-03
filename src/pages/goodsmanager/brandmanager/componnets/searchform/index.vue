7e266d99e427910e48caf9c2e5c0ce98439bb475
tiantangbuyaowo <954218947@qq.com>
2019-04-03 22:56:24 +0800

    添加功能完成

--- src/pages/goodsmanager/brandmanager/componnets/searchform/index.vue
+++ src/pages/goodsmanager/brandmanager/componnets/searchform/index.vue
@@ -1,68 +1,86 @@
 <template>
-  <el-form
-    :inline="true"
-    :model="form"
-    :rules="rules"
-    ref="form"
-    size="mini"
-    style="margin-bottom: -18px;">
+    <div style="margin-bottom: -18px;">
+        <el-form
+                :inline="true"
+                :model="form"
+                :rules="rules"
+                ref="form"
+                size="mini"
+        >
 
-    <el-form-item label="品牌名称" prop="name">
-      <el-input
-        v-model="form.name"
-        placeholder="请输入品牌名称"
-        style="width: 300px;"/>
-    </el-form-item>
-    <el-form-item>
-      <el-button
-        type="primary"
-        @click="handleFormSubmit">
-        <d2-icon name="search"/>
-        查询
-      </el-button>
-    </el-form-item>
+            <el-form-item label="品牌名称" prop="name">
+                <el-input
+                        v-model="form.name"
+                        placeholder="请输入品牌名称"
+                        style="width: 300px;"/>
+            </el-form-item>
+            <el-form-item>
+                <el-button
+                        type="primary"
+                        @click="handleFormSubmit">
+                    <d2-icon name="search"/>
+                    查询
+                </el-button>
+            </el-form-item>
 
-    <el-form-item>
-      <el-button
-        @click="handleFormReset">
-        <d2-icon name="refresh"/>
-        重置
-      </el-button>
-    </el-form-item>
+            <el-form-item>
+                <el-button
+                        @click="handleFormReset">
+                    <d2-icon name="refresh"/>
+                    重置
+                </el-button>
+            </el-form-item>
 
-  </el-form>
+        </el-form>
+
+
+        <el-form
+                :inline="true"
+                size="mini">
+            <el-form-item>
+                <el-button
+                        type="success"
+                        @click="showAddWin">
+                    <d2-icon name="plus"/>
+                    添加
+                </el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      form: {
-        user: ''
-      },
-      rules: {
-
-      }
-    }
-  },
-  methods: {
-    handleFormSubmit () {
-      this.$refs.form.validate((valid) => {
-        if (valid) {
-          this.$emit('submit', this.form)
+    export default {
+        data() {
+            return {
+                form: {
+                    user: ''
+                },
+                rules: {}
+            }
+        },
+        methods: {
+            handleFormSubmit() {
+                this.$refs.form.validate((valid) => {
+                    if (valid) {
+                        this.$emit('submit', this.form)
+                    }
+                    /*else {
+                      this.$notify.error({
+                        title: '错误',
+                        message: '表单校验失败'
+                      })
+                      return false
+                    }*/
+                })
+            },
+            handleFormReset() {
+                this.$refs.form.resetFields()
+            }
+            ,
+            showAddWin() {
+                this.$emit('showAddWin')
+            }
         }
-        /*else {
-          this.$notify.error({
-            title: '错误',
-            message: '表单校验失败'
-          })
-          return false
-        }*/
-      })
-    },
-    handleFormReset () {
-      this.$refs.form.resetFields()
     }
-  }
-}
 </script>
--- src/pages/goodsmanager/brandmanager/index.vue
+++ src/pages/goodsmanager/brandmanager/index.vue
@@ -3,7 +3,7 @@
 
     <d2-container :filename="filename">
         <template slot="header">
-            <searchForm @submit="handleSubmit"></searchForm>
+            <searchForm @submit="handleSubmit" @showAddWin="showAddWin"></searchForm>
         </template>
         <brandtTable
                 :table-data="table"
@@ -16,7 +16,24 @@
                 @change="handlePaginationChange"></paginationFooter>
 
 
+        <el-dialog title="添加品牌" :visible.sync="dialogAddFormVisible" :close-on-click-modal="false">
+            <el-form :model="addform" ref="addForm" :rules="addRules">
+                <el-form-item label="品牌名称" :label-width="formLabelWidth" prop="name">
+                    <el-input v-model="addform.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="品牌首字母" :label-width="formLabelWidth" prop="letter">
+                    <el-input v-model="addform.letter" autocomplete="off"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogAddFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="addBrand">确 定</el-button>
+            </div>
+        </el-dialog>
+
+
     </d2-container>
+
 
 </template>
 
@@ -40,6 +57,23 @@
                     pageSize: 10,
                     pageTotal: 0,
                     name: ''
+                },
+                dialogAddFormVisible: false,
+                addform: {
+                    name: '',
+                    letter: ''
+                },
+                formLabelWidth: '120px'
+                ,
+                addRules: {
+                    name: [
+                        {required: true, message: '请输入品牌名称', trigger: 'blur'},
+                        {min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur'}
+                    ],
+                    letter: [
+                        {required: true, message: '请输入品牌首字母', trigger: 'blur'},
+                        {min: 1, max: 1, message: '首字母只能是一个大写字母', pattern: /^[A-Z]+$/, trigger: 'blur'}
+                    ]
                 }
             }
         },
@@ -65,6 +99,14 @@
 
                 })
             },
+            addTableData: function () {
+                return request({
+                    url: '/goods/brand/brand',
+                    method: 'post',
+                    data: this.addform
+
+                })
+            },
             loadBrandTableData: function () {
                 this.getTableData().then(res => {
                     this.loading = false;
@@ -85,6 +127,41 @@
                 //alert(form.name);
                 this.page.name = form.name
                 this.loadBrandTableData();
+            },
+            showAddWin() {
+                //this.loading = true
+                this.dialogAddFormVisible = true
+
+            }, addBrand() {
+                //this.loading = true
+                this.$refs["addForm"].validate((valid) => {
+                    if (valid) {
+                        //alert('submit!');
+
+                        const formLoading = this.$loading({
+                            lock: true,
+                            text: '操作中',
+                            spinner: 'el-icon-loading',
+                            background: 'rgba(0, 0, 0, 0.7)'
+                        });
+
+                        this.addTableData().then(res => {
+                            formLoading.close();
+                            this.dialogAddFormVisible = false;
+                            this.loadBrandTableData();
+
+                        })
+                            .catch(err => {
+                                formLoading.close();
+                            })
+
+
+                    } else {
+                        //console.log('error submit!!');
+                        return false;
+                    }
+                });
+
             }
         }
     }

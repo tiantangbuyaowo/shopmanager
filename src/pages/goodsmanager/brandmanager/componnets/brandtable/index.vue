61bdf852b16154c959d1762e0b59d2e0f318a6b4
tiantangbuyaowo <954218947@qq.com>
2019-05-15 22:10:37 +0800

    查询条件完成

--- src/pages/goodsmanager/brandmanager/componnets/brandtable/index.vue
+++ src/pages/goodsmanager/brandmanager/componnets/brandtable/index.vue
@@ -30,6 +30,19 @@
                     <img :src="scope.row.image" width="100%">
                 </template>
             </el-table-column>
+            <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <el-button type="primary"
+                               size="mini"
+                               @click="handleEdit(scope.row)" icon="el-icon-edit">编辑
+                    </el-button>
+                    <el-button
+                            size="mini"
+                            type="danger" icon="el-icon-delete"
+                            @click="handleDelete(scope.row)">删除
+                    </el-button>
+                </template>
+            </el-table-column>
         </el-table>
     </div>
 </template>
@@ -47,66 +60,25 @@
         },
         data() {
             return {
-                currentTableData: [],
-                multipleSelection: [],
-                downloadColumns: [
-                    {label: '卡密', prop: 'name'},
-                    {label: '面值', prop: 'value'},
-                    {label: '状态', prop: 'type'},
-                    {label: '管理员', prop: 'admin'},
-                    {label: '管理员备注', prop: 'adminNote'},
-                    {label: '创建时间', prop: 'dateTimeCreat'},
-                    {label: '使用状态', prop: 'used'},
-                    {label: '使用时间', prop: 'dateTimeUse'}
-                ]
+                currentTableData: []
+
             }
-        },
+        }
+   ,
         watch: {
             tableData: {
                 handler(val) {
+
                     this.currentTableData = val
+
                 },
                 immediate: true
             }
-        },
+        }
+    ,
         methods: {
-            handleSwitchChange(val, index) {
-                const oldValue = this.currentTableData[index]
-                this.$set(this.currentTableData, index, {
-                    ...oldValue,
-                    type: val
-                })
-                // 注意 这里并没有把修改后的数据传递出去 如果需要的话请自行修改
-            },
-            handleSelectionChange(val) {
-                this.multipleSelection = val
-            },
-            downloadDataTranslate(data) {
-                return data.map(row => ({
-                    ...row,
-                    type: row.type ? '禁用' : '正常',
-                    used: row.used ? '已使用' : '未使用'
-                }))
-            },
-            handleDownloadXlsx(data) {
-                this.$export.excel({
-                    title: 'D2Admin 表格示例',
-                    columns: this.downloadColumns,
-                    data: this.downloadDataTranslate(data)
-                })
-                    .then(() => {
-                        this.$message('导出表格成功')
-                    })
-            },
-            handleDownloadCsv(data) {
-                this.$export.csv({
-                    title: 'D2Admin 表格示例',
-                    columns: this.downloadColumns,
-                    data: this.downloadDataTranslate(data)
-                })
-                    .then(() => {
-                        this.$message('导出CSV成功')
-                    })
+            handleDelete(row) {
+                this.$emit('deleteRow', row)
             }
         }
     }
